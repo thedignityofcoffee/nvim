5b67cbd5b4efe2a48e705878d1058eeb2ab4dbd7
Bertrand Yuan <thedignityofcoffee@outlook.com>
2025-06-03 23:22:45 +0800

    add keymaps for fzf.lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -4,5 +4,18 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
-  opts = {}
+  opts = {},
+  config = function()
+    local fzf = require("fzf-lua")
+    fzf.setup({}) -- 可加自定义配置
+
+    -- 常用映射
+    vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Buffers" })
+    vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Help Tags" })
+    vim.keymap.set("n", "<leader>fc", fzf.commands, { desc = "Commands" })
+    vim.keymap.set("n", "<leader>fo", fzf.oldfiles, { desc = "Old Files" })
+  end,
 }
+
